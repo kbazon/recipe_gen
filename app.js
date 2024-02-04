8dd5d461a775a1146cd30c765be9e45bbacf448b
kbazon <karenbazon20@gmail.com>
2024-02-04 02:33:06 +0100

    modified:   app.js

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const port = 3000; // Change as needed
+const port = 3000; 
 
-const spoonacularApiKey = 'c69613fec1914093b5aae817fafaf289'; // Replace with your actual Spoonacular API key
+const spoonacularApiKey = 'c69613fec1914093b5aae817fafaf289'; 
 const spoonacularApiUrl = 'https://api.spoonacular.com/recipes/complexSearch';
 
 app.use(express.json());
 
 app.get('/recipes', async (req, res) => {
-  const query = req.query.query || ''; // Default to an empty string if not provided
+  const query = req.query.query || ''; 
 
   try {
     const response = await axios.get(`${spoonacularApiUrl}?apiKey=${spoonacularApiKey}&query=${query}`);
@@ -21,7 +21,7 @@ app.get('/recipes', async (req, res) => {
         sourceUrl: recipe.sourceUrl,
       }));
 
-      // Create an HTML page
+      
       const htmlPage = `
         <!DOCTYPE html>
         <html lang="en">
@@ -53,3 +53,4 @@ app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
+//http://localhost:3000/recipes?query='ingredient'
